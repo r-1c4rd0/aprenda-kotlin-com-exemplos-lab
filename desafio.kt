b7ee32e857bb21f858e7e52f82c684e637fc5e46
Ricardo Santiago <ricardosantiagocwb@gmail.com>
2024-01-11 17:19:44 -0300

    Resolução do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,8 +1,10 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
+import java.util.UUID
+
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val id: UUID = UUID.randomUUID())
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
@@ -11,11 +13,32 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val basico = Nivel.BASICO
+    val intermediario = Nivel.INTERMEDIARIO
+    val difícil = Nivel.DIFICIL
+
+    val basicoIngles = ConteudoEducacional("Inglês Básico", 120)
+    val intermediarioIngles = ConteudoEducacional("Inglês Intermediário", 120)
+    val avancadoIngles = ConteudoEducacional("Inglês Avançado", 120)
+
+    val basicoPortugues = ConteudoEducacional("Português Básico", 120)
+    val intermediarioPortugues = ConteudoEducacional("Português Intermediário", 120)
+    val avancadoPortugues = ConteudoEducacional("Português Avançado", 120)
+
+    val formacaoIngles = Formacao("Inglês", listOf(basicoIngles, intermediarioIngles, avancadoIngles))
+    val formacaoPortugues = Formacao("Português", listOf(basicoPortugues, intermediarioPortugues, avancadoPortugues))
+
+    val aluno1 = Usuario()
+    val aluno2 = Usuario()
+
+    formacaoIngles.matricular(aluno1)
+    formacaoPortugues.matricular(aluno2)
+
+    println("Aluno1 matriculado na Formação de Inglês: ${formacaoIngles.inscritos.contains(aluno1)}")
+    println("Aluno2 matriculado na Formação de Português: ${formacaoPortugues.inscritos.contains(aluno2)}")
 }
